e1f6acd7219ec5526dd719a04a23eec06ef9b64c
Rémi Durieu <remi.durieu@univ-rouen.fr>
2020-07-07 20:30:25 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ echo "Installing dotrs..."
 
 if [[ $1 == "--local" ]]; then
   bin_dir=$HOME/.local/bin
+  lib_dir=$HOME/.local/share/dotrs
   mkdir -p $bin_dir
+  mkdir -p $lib_dir
 else
   if [[ $EUID -ne 0 ]]; then
     echo "Error: this script requires root privileges to install dotrs."
@@ -12,10 +14,10 @@ else
     exit 1
   fi
   bin_dir=/usr/bin
+  lib_dir=/usr/lib/dotrs
+  mkdir -p $lib_dir
 fi
 
-lib_dir=$HOME/.local/share/dotrs
-mkdir -p $lib_dir
 cp *.rb $lib_dir
 chmod +x $lib_dir/dotrs.rb
 ln -s $lib_dir/dotrs.rb $bin_dir/dotrs
